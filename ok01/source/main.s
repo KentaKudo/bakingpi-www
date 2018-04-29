36ad496e2d4504cffae999f98f24b3958decc98c
Kenta Kudo <yd37574@gmail.com>
2018-04-29 09:11:45 +0100

    Add: comment

--- ok01/source/main.s
+++ ok01/source/main.s
@@ -1,12 +1,15 @@
+/* With the help of the following threads */
+/* https://github.com/Chadderz121/bakingpi-www/issues/12 */
+/* https://www.raspberrypi.org/forums/viewtopic.php?f=72&t=114796 */
 .section .init
 .global _start
 _start:
           ldr r0,=0x20200000
           mov r1,#1
           lsl r1,#21
-          str r1,[r0,#16]
+          str r1,[r0,#16] /* ACT is moved to 47th pin */
           mov r1,#1
-          lsl r1,#15
+          lsl r1,#15 /* use GPSET1 instead of GPSET0 */
           str r1,[r0,#0x20]
 loop$:
 b loop$
