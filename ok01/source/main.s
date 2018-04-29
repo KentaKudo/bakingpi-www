b0fc482b01aefabc1e4ac04dae2387aaa118b0dc
Kenta Kudo <yd37574@gmail.com>
2018-04-29 09:06:56 +0100

    ok1

---
+++ ok01/source/main.s
@@ -0,0 +1,13 @@
+.section .init
+.global _start
+_start:
+          ldr r0,=0x20200000
+          mov r1,#1
+          lsl r1,#21
+          str r1,[r0,#16]
+          mov r1,#1
+          lsl r1,#15
+          str r1,[r0,#0x20]
+loop$:
+b loop$
+
