dca420bf0fad21b9ec555429711d73810e55e040
Kenta Kudo <yd37574@gmail.com>
2018-05-14 19:55:54 +0100

    screen03

---
+++ screen03/source/main.s
@@ -0,0 +1,51 @@
+.section .init
+.global _start
+_start:
+
+b main
+
+
+.section .text
+main:
+    mov sp,#0x8000
+
+    mov r0,#1024
+    mov r1,#768
+    mov r2,#16
+    bl InitialiseFrameBuffer
+
+    teq r0,#0
+    bne noError$
+
+    mov r0,#47
+    mov r1,#1
+    bl SetGpioFunction
+
+    mov r0,#47
+    mov r1,#0
+    bl SetGpio
+
+    error$:
+        b error$
+
+    noError$:
+
+    fbInfoAddr .req r4
+    mov fbInfoAddr,r0
+
+    bl SetGraphicsAddress
+
+    mov r0,#9
+    bl FindTag
+
+    ldr r1,[r0]
+    lsl r1,#2
+    sub r1,#8
+    add r0,#8
+
+    mov r2,#0
+    mov r3,#0
+    bl DrawString
+
+loop$:
+    b loop$
